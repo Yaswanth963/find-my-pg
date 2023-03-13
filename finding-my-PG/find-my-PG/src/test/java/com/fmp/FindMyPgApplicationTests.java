167c1da32d67b70b7d448e758d424979614614f6
Yaswanth963 <perumallayaswanth2000@gmail.com>
2023-03-13 23:22:46 +0530

    project-setup

---
+++ finding-my-PG/find-my-PG/src/test/java/com/fmp/FindMyPgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindMyPgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ finding-my-PG/src/main/java/com/fmp/FindingMyPgApplication.java
@@ -0,0 +1,13 @@
+package com.fmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindingMyPgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindingMyPgApplication.class, args);
+	}
+
+}
